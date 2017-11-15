c190e52e02ffab204f9070b39b38817085563ecb
Christian Herberger <herberger@punkt.de>
2017-11-15 15:45:53 +0100

    TASK: initialize package

---
+++ Classes/Context/ApiTestingContext.php
@@ -0,0 +1,12 @@
+<?php
+namespace PunktDe\Testing\Api\Context;
+
+/*
+ * This file is part of the PunktDe.Testing.Api package.
+ */
+
+use Behat\Behat\Context\Context;
+
+class ApiTestingContext implements Context {
+
+}
